b7f1046f9d2120b8aca1941841e5b1686791280f
Sourav Chhimpa <souravchhimpa1@gmail.com>
2024-10-11 15:20:34 +0530

    page not found some update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -15,7 +15,7 @@ const NotFound: React.FC = () => {
       <div className="mt-8">
         <h2 className="text-2xl font-semibold mb-2">Developer Section</h2>
         <p className="text-base text-gray-400 mb-4 px-4 mt-3">
-          This Website was crafted by Sourav Chhimpa, a freelance web developer from India.
+          This Website was crafted by <span className='text-gray-100 font-semibold'>Sourav Chhimpa</span>, a freelance web developer from India.
         </p>
         <div className="flex space-x-4 justify-center">
           <a
